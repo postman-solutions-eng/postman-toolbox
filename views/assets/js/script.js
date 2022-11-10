706a485ec0821a9b367d818c66b353604dc6d493
Jordan Walsh <jordan.walsh@postman.com>
2022-11-10 12:06:55 +1100

    Added autocompletion logic
    Also added better error handling for Aliases and logging for analytics.

--- views/assets/js/script.js
+++ views/assets/js/script.js
@@ -5,16 +5,15 @@
  */
 
 async function validate () {
-
   //Hide copy buttons
-  document.getElementById("jsonSchemaCopy").style.display = "none";
-  document.getElementById("spectralResultCopy").style.display = "none";
+  document.getElementById('jsonSchemaCopy').style.display = 'none'
+  document.getElementById('spectralResultCopy').style.display = 'none'
 
-  document.getElementById("jsonPathResult").innerHTML = "Validating...";
-  document.getElementById("spectralResult").innerHTML = "Validating...";
+  document.getElementById('jsonPathResult').innerHTML = 'Validating...'
+  document.getElementById('spectralResult').innerHTML = 'Validating...'
 
-  let spectralRule = spectralRuleEditor.getValue(); 
-  let openApiSpec = openApiSpecEditor.getValue(); 
+  let spectralRule = editors[0].getValue()
+  let openApiSpec = editors[1].getValue()
 
   let formBody = [
     `spectralRule=${encodeURIComponent(spectralRule)}`,
@@ -32,68 +31,76 @@ async function validate () {
   })
     .then(response => response.json())
     .then(data => {
-
-      if(data.error && data.error == 500) {
-        throw data.message;
+      if (data.error && data.error == 500) {
+        throw data.message
       }
 
-      document.getElementById("jsonPathResult").innerHTML = JSON.stringify(data.jsonPathMatches, null, 2);
+      document.getElementById('jsonPathResult').innerHTML = JSON.stringify(
+        data.jsonPathMatches,
+        null,
+        2
+      )
 
-      if(!data.spectralResults || data.spectralResults.length == 0) {
-        document.getElementById("spectralResult").innerHTML = "No issues found!";
-        document.getElementById("jsonSchemaCopy").style.display = "block";
-
+      if (!data.spectralResults || data.spectralResults.length == 0) {
+        document.getElementById('spectralResult').innerHTML = 'No issues found!'
+        document.getElementById('jsonSchemaCopy').style.display = 'block'
       } else {
         //Show copy buttons
-        document.getElementById("jsonSchemaCopy").style.display = "block";
-        document.getElementById("spectralResultCopy").style.display = "block";
+        document.getElementById('jsonSchemaCopy').style.display = 'block'
+        document.getElementById('spectralResultCopy').style.display = 'block'
 
-        let message = `${data.spectralResults.length} issue found`;
-        if(data.spectralResults.length > 1) {
-          message = `${data.spectralResults.length} issues found`;
+        let message = `${data.spectralResults.length} issue found`
+        if (data.spectralResults.length > 1) {
+          message = `${data.spectralResults.length} issues found`
         }
 
         let results = {
-          "summary" : message,
-          "results" : data.spectralResults
+          summary: message,
+          results: data.spectralResults
         }
 
         //document.getElementById("spectralResult").parentNode.innerText = `${message}`;
-        document.getElementById("spectralResult").innerHTML = JSON.stringify(results, null, 2);
+        document.getElementById('spectralResult').innerHTML = JSON.stringify(
+          results,
+          null,
+          2
+        )
       }
-
     })
     .catch(message => {
-      document.getElementById("jsonPathResult").innerHTML = message;
-      document.getElementById("spectralResult").innerHTML = message;
+      document.getElementById('jsonPathResult').innerHTML = message
+      document.getElementById('spectralResult').innerHTML = message
     })
 }
 
-//Set up the editors.
-var spectralRuleEditor = ace.edit("spectralRule");
-spectralRuleEditor.setTheme("ace/theme/twilight");
-spectralRuleEditor.session.setMode("ace/mode/yaml");
-spectralRuleEditor.session.setTabSize(2);
-spectralRuleEditor.session.setUseSoftTabs(true);
+let editorNames = ['spectralRule', 'openApiSpec']
+let editors = []
 
-var openApiSpecEditor = ace.edit("openApiSpec");
-openApiSpecEditor.setTheme("ace/theme/twilight");
-openApiSpecEditor.session.setMode("ace/mode/yaml");
-openApiSpecEditor.session.setTabSize(2);
-openApiSpecEditor.session.setUseSoftTabs(true);
+for (let editor of editorNames) {
+  ace.require("ace/ext/language_tools");
+  var thisEditor = ace.edit(editor)
+  thisEditor.setTheme('ace/theme/twilight')
+  thisEditor.session.setMode('ace/mode/yaml')
+  thisEditor.session.setTabSize(2)
+  thisEditor.session.setUseSoftTabs(true)
+  thisEditor.setOptions({
+    enableBasicAutocompletion: true,
+    enableSnippets: true,
+    enableLiveAutocompletion: false
+  })
+  editors.push(thisEditor)
+}
 
 //Attach a listener to the copy buttons
-for(let element of document.getElementsByClassName("copy")) {
-  element.addEventListener("click", function(evt) {
-
+for (let element of document.getElementsByClassName('copy')) {
+  element.addEventListener('click', function (evt) {
     // Copy the text inside the text field
-    navigator.clipboard.writeText(evt.target.nextSibling.innerText);
-    
-    evt.target.innerText = "Copied"
-    
-    setTimeout(() => {
-      evt.target.innerText = "Copy"
-    },3000)
+    navigator.clipboard.writeText(evt.target.nextSibling.innerText)
 
+    evt.target.innerText = 'Copied'
+
+    setTimeout(() => {
+      evt.target.innerText = 'Copy'
+    }, 3000)
   })
 }
